bff1ff0e7413e054667421543d365595f62d024a
satoshi katayama <3104katayama@gmail.com>
2017-03-26 22:52:26 +0900

    add

--- app/assets/javascripts/partials/common.js
+++ app/assets/javascripts/partials/common.js
@@ -1,31 +1,40 @@
 jQuery(function($) {
-	var nav = $('#global-navi'),
-	offset = nav.offset();
-	$(window).scroll(function () {
-	  if($(window).scrollTop() > offset.top) {
-	    nav.addClass('fixed');
-	  } else {
-	    nav.removeClass('fixed');
-	  }
-	});
+  var nav = $('#global-navi'),
+    offset = nav.offset();
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > offset.top) {
+      nav.addClass('fixed');
+    } else {
+      nav.removeClass('fixed');
+    }
+  });
 });
 
 $(function() {
-    var topBtn = $('#page-top');
-    topBtn.hide();
-    //スクロールが100に達したらボタン表示
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            topBtn.fadeIn();
-        } else {
-            topBtn.fadeOut();
-        }
-    });
-    //スクロールしてトップ
-    topBtn.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
+  var topBtn = $('#page-top');
+  topBtn.hide();
+  //スクロールが100に達したらボタン表示
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+  //スクロールしてトップ
+  topBtn.click(function() {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
+  $('.banner-bottom').overlayAd({
+    'position': 'bottom',
+    'mobileStyle': 'trimming',
+    'targetBlank': false,
+    'backgroundStyle': true,
+    'backgroundColor': 'none'
+  });
+
 });
