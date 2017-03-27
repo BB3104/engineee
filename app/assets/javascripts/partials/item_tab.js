294f56e9307cdd09c6ff3eeca15d8ddc3110e202
satoshi katayama <3104katayama@gmail.com>
2017-03-27 23:34:59 +0900

    add

--- app/assets/javascripts/partials/item_tab.js
+++ app/assets/javascripts/partials/item_tab.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         pager: false,
         controls: false,
         touchEnabled: true, // モバイルのスワイプ
-        swipeThreshold: 80, //スワイプ操作として処理される最小の移動距離
+        swipeThreshold: 70, //スワイプ操作として処理される最小の移動距離
         // preventDefaultSwipeY:false,
         preloadImages: 'visible',
         // preventDefaultSwipeX: false,
